b67f781d7dc81d769b25b54833d87b51380647d4
Samuel Ryan <sam@shr.ink>
2024-09-07 02:26:18 +0700

    feat: Add warning about breaking change

--- src/extract.js
+++ src/extract.js
@@ -7,6 +7,14 @@ async function run() {
     const path = core.getInput('path');
     const destination = core.getInput('destination') || `extracted-${Date.now()}`;
 
+    if (!core.getInput('destination')) {
+      core.notice([
+        'As you did not specify a destination, the default is being used.',
+        'As of version shrink/actions-docker-extract@3.0.1, the default does not include a dot prefix.',
+        'See https://github.com/shrink/actions-docker-extract/issues/28 for context.',
+      ].join(' '));
+    }
+
     const create = `docker cp $(docker create ${image}):/${path} ${destination}`;
 
     await exec.exec(`mkdir -p ${destination}`);
